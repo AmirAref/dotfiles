2f9a10447238ae24aaa26bdc032d5148a8535666
Amir Aref <amiraref808@gmail.com>
2022-05-26 09:33:50 +0430

    use the abbreviation instead the alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,9 @@
-# aliases 
-source ~/.shell_aliases
-
 set fish_greeting
+
+# aliases 
+# source ~/.shell_aliases
+
+# abbreviations
+abbr --add gs git status
+abbr --add cfg /usr/bin/git --work-tree=$HOME --git-dir=$HOME/.cfg
+
