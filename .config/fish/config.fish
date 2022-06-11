25fee0873f036c988a4e7f0c4dbf161c2acbbd79
Amir Aref <amiraref808@gmail.com>
2022-06-11 18:50:19 +0430

    update fish shell config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,9 @@ set fish_greeting
 
 # abbreviations
 abbr --add gs git status
+abbr --add gl git log 
+abbr --add glo git log --oneline --all --graph
+abbr --add ga git add
+abbr --add gc git commit
 abbr --add cfg /usr/bin/git --work-tree=$HOME --git-dir=$HOME/.cfg
-
+abbr --add cl clear
