df92481a0278ad6b90aeb8b07156f4090a01d180
Amir Aref <amiraref808@gmail.com>
2022-06-19 18:43:30 +0430

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,19 @@ abbr --add ga git add
 abbr --add gc git commit
 abbr --add cfg /usr/bin/git --work-tree=$HOME --git-dir=$HOME/.cfg
 abbr --add cl clear
+abbr --add jn jupyter-notebook
+
+
+# use the last command by !! 
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+end
