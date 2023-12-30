89b3dde33742411ddf0c6f0c74e6b32cf3009fdb
Amir Aref <amiraref808@gmail.com>
2023-12-30 12:01:28 +0330

    update fish shell and moc config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 set fish_greeting
 
 # aliases 
-source ~/.shell_aliases
+# source ~/.shell_aliases
 
 # abbreviations
 
@@ -21,10 +24,11 @@ abbr --add unset set --erase
 # cusotm
 abbr --add cfg /usr/bin/git --work-tree=$HOME --git-dir=$HOME/.dotfiles
 abbr --add jn jupyter-notebook
-abbr --add subl LANG=en_US.UTF-8 LC_TYPE=en_US.UTF-8 subl
+abbr --add ff fastfetch
+abbr --add kdrf kdeconnect-cli --refresh
+#abbr --add subl LANG=en_US.UTF-8 LC_TYPE=en_US.UTF-8 subl
 
-
-# use the last command by !! 
+# use the last command by !!
 function bind_bang
     switch (commandline -t)[-1]
         case "!"
@@ -41,9 +45,11 @@ end
 # varibales
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-
+abbr --add enable_proxy "export {all_proxy,ALL_PROXY,http_proxy,HTTP_PROXY}=\"http://127.0.0.1:2081\""
+abbr --add disable_proxy set --erase {all_proxy,ALL_PROXY,http_proxy,HTTP_PROXY}
 #export {all_proxy,ALL_PROXY,http_proxy,HTTP_PROXY}=http://127.0.0.1:2081
 export {no_proxy,NO_PROXY}=127.0.0.1,localhost
 
 # add PATH
 export PATH="/home/amir/.local/bin:$PATH"
+
