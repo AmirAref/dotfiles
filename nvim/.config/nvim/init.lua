8e93411d75c82e8e6507740d9698d44f9af98d0a
Amir Aref <amiraref808@gmail.com>
2024-07-25 07:09:39 +0330

    nvim: update which-key plugin config in neovim
    add thunar to hyprland-pkglist file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -290,25 +290,35 @@ require('lazy').setup({
 
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+    event = 'VeryLazy',
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>c_', hidden = true },
 
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
-    end,
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>d_', hidden = true },
+
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>r_', hidden = true },
+
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>s_', hidden = true },
+
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>w_', hidden = true },
+
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>t_', hidden = true },
+
+      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      { '<leader>h_', hidden = true },
+    },
+    -- visual mode
   },
 
   -- NOTE: Plugins can specify dependencies.
